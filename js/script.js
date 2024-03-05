44a924103eee33967dda9112f077467f96c688f9
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-05 14:33:47 +0100

    [add] Cards

--- js/script.js
+++ js/script.js
@@ -1,40 +1,50 @@
+const output = document.querySelector('.output');
 const team = [
   {
   firstName: 'Wayne',
   lastName: 'Barnett',
   role: 'Founder & CEO',
   photo: 'img/wayne-barnett-founder-ceo.jpg',
-},
-{
+  },
+  {
   firstName: 'Angela',
   lastName: 'Caroll',
   role: 'Chief Editor',
   photo: 'img/angela-lopez-social-media-manager.jpg',
-},
-{
+  },
+  {
   firstName: 'Walter',
   lastName: 'Gordon',
   role: 'Office Manager',
   photo: 'img/walter-gordon-office-manager.jpg',
-},
-{
+  },
+  {
   firstName: 'Angela',
   lastName: 'Lopez',
   role: 'Social Media Manager',
   photo: 'img/angela-caroll-chief-editor.jpg',
-},
-{
+  },
+  {
   firstName: 'Scott',
   lastName: 'Estrada',
   role: 'Developer',
   photo: 'img/scott-estrada-developer.jpg',
-},
-{
+  },
+  {
   firstName: 'Barbara',
   lastName: 'Ramos',
   role: 'Graphic Designer',
   photo: 'img/barbara-ramos-graphic-designer.jpg',
   }
-  
 ] 
-console.log(team);
+
+for(let worker of team){
+  output.innerHTML +=`
+  <div class="card m-2  text-center" style="width: 30%">
+    <img src="${worker.photo}" class="card-img-top" alt="${worker.firstName}">
+    <div class="card-body">
+      <h5 class="card-title">${worker.firstName, worker.lastName}</h5>
+      <p class="card-text">${worker.role}</p>
+    </div>
+  </div>`
+}
