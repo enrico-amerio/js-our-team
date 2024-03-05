f5a2f1eea22c9d166e1f811d8788486e82962656
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-05 13:40:12 +0100

    [add] Picture to each team member in the array team

--- js/script.js
+++ js/script.js
@@ -3,31 +3,37 @@ const team = [
   firstName: 'Wayne',
   lastName: 'Barnett',
   role: 'Founder & CEO',
-  },
-  {
+  photo: 'img/wayne-barnett-founder-ceo.jpg',
+},
+{
   firstName: 'Angela',
   lastName: 'Caroll',
   role: 'Chief Editor',
-  },
-  {
+  photo: 'img/angela-lopez-social-media-manager.jpg',
+},
+{
   firstName: 'Walter',
   lastName: 'Gordon',
   role: 'Office Manager',
-  },
-  {
+  photo: 'img/walter-gordon-office-manager.jpg',
+},
+{
   firstName: 'Angela',
   lastName: 'Lopez',
   role: 'Social Media Manager',
-  },
-  {
+  photo: 'img/angela-caroll-chief-editor.jpg',
+},
+{
   firstName: 'Scott',
   lastName: 'Estrada',
   role: 'Developer',
-  },
-  {
+  photo: 'img/scott-estrada-developer.jpg',
+},
+{
   firstName: 'Barbara',
   lastName: 'Ramos',
   role: 'Graphic Designer',
+  photo: 'img/barbara-ramos-graphic-designer.jpg',
   }
   
 ] 
