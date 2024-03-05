bdba29b33fc05a70b75b0513a13bd5f389f83a84
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-05 13:35:53 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const team = [
+  {
+  firstName: 'Wayne',
+  lastName: 'Barnett',
+  role: 'Founder & CEO',
+  },
+  {
+  firstName: 'Angela',
+  lastName: 'Caroll',
+  role: 'Chief Editor',
+  },
+  {
+  firstName: 'Walter',
+  lastName: 'Gordon',
+  role: 'Office Manager',
+  },
+  {
+  firstName: 'Angela',
+  lastName: 'Lopez',
+  role: 'Social Media Manager',
+  },
+  {
+  firstName: 'Scott',
+  lastName: 'Estrada',
+  role: 'Developer',
+  },
+  {
+  firstName: 'Barbara',
+  lastName: 'Ramos',
+  role: 'Graphic Designer',
+  }
+  
+] 
+console.log(team);
